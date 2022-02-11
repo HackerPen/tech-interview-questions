7a9181fa50280e98c2fe7c4878a5c822b669ca47
Alexander Wang <alexander.wang22@gmail.com>
2022-02-11 14:49:26 -0500

    added javascript and python solutions for find median from data stream

---
+++ coding/find_median_from_data_stream/starter.js
@@ -0,0 +1,24 @@
+class MedianFinder {
+  /**
+   * initialize your data structure here.
+   */
+  constructor() {}
+
+  /**
+   * @param {number} num
+   * @return {void}
+   */
+  addNum(num) {}
+
+  /**
+   * @return {number}
+   */
+  findMedian() {}
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
---
+++ coding/find_median_from_data_stream/starter.py
@@ -0,0 +1,16 @@
+class MedianFinder:
+
+    def __init__(self):
+
+
+    def addNum(self, num: int) -> None:
+
+
+    def findMedian(self) -> float:
+
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
