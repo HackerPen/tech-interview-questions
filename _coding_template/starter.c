019b1b665619f193a0ecfa5ec94e656d97aac9b2
alexanderywang <58057993+alexanderywang@users.noreply.github.com>
2022-02-13 15:49:32 -0500

    starter code filled out and meta updated (#21)

---
+++ _coding_template/starter.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize){
+
+}
---
+++ coding/merge_k_sorted_lists/starter.rb
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode[]} lists
+# @return {ListNode}
+def merge_k_lists(lists)
+
+end
