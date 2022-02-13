06efb5a33404b6498c8a994bf3a5c38515bee9f1
alexanderywang <58057993+alexanderywang@users.noreply.github.com>
2022-02-13 15:48:06 -0500

    starter code filled out and meta updated (#23)

---
+++ coding/top_k_frequent_elements/starter.c
@@ -0,0 +1,8 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize){
+
+}
---
+++ coding/top_k_frequent_elements/starter.go
@@ -0,0 +1,3 @@
+func topKFrequent(nums []int, k int) []int {
+
+}
