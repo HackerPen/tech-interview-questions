9cf3a810abe891c428b4058589617570ccd46b9e
Arijit1000 <73710898+Arijit1000@users.noreply.github.com>
2022-02-16 02:25:48 +0530

    Update starter.java

--- coding/merge_k_sorted_lists/starter.java
+++ coding/merge_k_sorted_lists/starter.java
@@ -10,6 +10,26 @@
  */
 class Solution {
   public ListNode mergeKLists(ListNode[] lists) {
+    
+     if (lists == null || lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, (ListNode a, ListNode b) -> (a.val - b.val));
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+        ListNode dummy = new ListNode(0), tail = dummy;
+        while (!pq.isEmpty()) {
+            ListNode curNode = pq.poll();
+            tail.next = curNode;
+            tail = tail.next;
+            if (curNode.next != null) {
+                pq.offer(curNode.next);
+            }
+        }
+        return dummy.next;
 
   }
 }
