d058cd04ee1772ffadd2a4e2e5569f4fa5652479
Alexander Wang <alexander.wang22@gmail.com>
2022-02-20 18:25:24 -0500

    added reverse linked list coding question

---
+++ coding/reverse_linked_list/starter.go
@@ -0,0 +1,10 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func reverseLinkedList(head *ListNode) *ListNode {
+
+}
---
+++ coding/reverse_linked_list/starter.rb
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_linked_list(head)
+
+end
