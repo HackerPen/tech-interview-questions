129fe772356f72cce99cb7ef44f41df9cb37b4ad
Alexander Wang <alexander.wang22@gmail.com>
2022-02-20 19:20:10 -0500

    added merge 2 sorted linked lists coding question

---
+++ coding/merge_two_sorted_linked_lists/starter.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+
+}
---
+++ coding/merge_two_sorted_linked_lists/starter.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const mergeTwoLists = (l1, l2) => {
+
+}
