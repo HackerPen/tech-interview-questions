c7493a1bc4c433685a28aafdc9daa4be3ee2f6bd
Arijit1000 <73710898+Arijit1000@users.noreply.github.com>
2022-02-27 00:30:05 +0530

    Update Valid Anagram

--- coding/valid_anagram/starter.java
+++ coding/valid_anagram/starter.java
@@ -1,5 +1,15 @@
 class Solution {
   public boolean isValidAnagram(String s, String t) {
+    
+    Map<Character,Integer> m1=new HashMap<>();
+        for(int i=0;i<s.length();i++) 
+        m1.put(s.charAt(i),m1.getOrDefault(s.charAt(i),0)+1);
+         for(int i=0;i<t.length();i++) 
+        m1.put(t.charAt(i),m1.getOrDefault(t.charAt(i),0)-1);
+        for(int val:m1.values())
+            if(val!=0)
+                return false;
+        return true;
 
   }
 }
