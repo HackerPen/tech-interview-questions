c8dd6066830c9a16505f273f860b6c95aab5072d
Marshall Shen <shen.marshall@gmail.com>
2022-03-01 20:44:17 -0600

    add longest substring

---
+++ coding/longest_substring_without_repeating_characters/starter.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
---
+++ coding/longest_substring_without_repeating_characters/starter.rb
@@ -0,0 +1,5 @@
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+
+end
